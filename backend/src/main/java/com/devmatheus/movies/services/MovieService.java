091556b16231845ea7a2114081ac3b331fdd186e
matheus-mag <matheusmag1103@gmail.com>
2022-03-17 21:44:36 -0300

    Busca de filmes

---
+++ backend/src/main/java/com/devmatheus/movies/services/MovieService.java
@@ -0,0 +1,36 @@
+package com.devmatheus.movies.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devmatheus.movies.dto.MovieDTO;
+import com.devmatheus.movies.entities.Movie;
+import com.devmatheus.movies.repositories.MovieRepository;
+
+@Service
+public class MovieService {
+	
+	@Autowired
+	private MovieRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findAll(Pageable pageable){
+		
+		Page<Movie> result = repository.findAll(pageable);
+		Page<MovieDTO> page = result.map(x -> new MovieDTO(x));
+		return page;
+	}
+	
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id){
+		
+		Movie result = repository.findById(id).get();
+		MovieDTO dto = new MovieDTO(result);
+		return dto;
+	}
+}

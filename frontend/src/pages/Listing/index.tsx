f368a6b9d0e0234da23f5e57931776cbd04da8ce
matheus-mag <matheusmag1103@gmail.com>
2022-03-30 19:25:47 -0300

    useParams

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,7 +22,7 @@ function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
             .then(resposta => {
                 const data = resposta.data as MoviePage;
                 setPage(data);
